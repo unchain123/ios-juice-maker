94fe3a8c588f2714e3854bdd86f442496307ab73
unchain123 <unchain123@kakao.com>
2023-03-17 13:39:20 +0900

    refactor: 기존 모델 변경

--- JuiceMaker/JuiceMaker/Model/Fruit.swift
+++ JuiceMaker/JuiceMaker/Model/Fruit.swift
@@ -5,11 +5,36 @@
 //  Created by Kiwi, unchain123 on 2022/04/26.
 //
 
-enum Fruit: CaseIterable {
-    case strawberry
-    case banana
-    case pineapple
-    case kiwi
-    case mango
+enum Fruit: String, CaseIterable, Comparable {
+    static func < (lhs: Fruit, rhs: Fruit) -> Bool {
+        lhs.rawValue < rhs.rawValue
+    }
+
+  case strawberry
+  case apple
+  case watermelon
+  case banana
+  case mango
+  case peach
+  case kiwi
+
+  var icon: String {
+    switch self {
+    case .strawberry:
+      return "🍓"
+    case .apple:
+      return "🍏"
+    case .watermelon:
+      return "🍉"
+    case .banana:
+      return "🍌"
+    case .mango:
+      return "🥭"
+    case .peach:
+      return "🍑"
+    case .kiwi:
+      return "🥝"
+    }
+  }
 }
 
--- JuiceMaker/JuiceMaker/Model/Juice.swift
+++ JuiceMaker/JuiceMaker/Model/Juice.swift
@@ -7,51 +7,7 @@
 
 import Foundation
 
-enum Juice {
-    case strawberry
-    case banana
-    case pineapple
-    case kiwi
-    case mango
-    case strawberryBanana
-    case mangoKiwi
-    
-    var name: String {
-        switch self {
-        case .strawberry:
-            return "딸기쥬스"
-        case .banana:
-            return "바나나쥬스"
-        case .pineapple:
-            return "파인애플쥬스"
-        case .kiwi:
-            return "키위쥬스"
-        case .mango:
-            return "망고쥬스"
-        case .strawberryBanana:
-            return "딸바쥬스"
-        case .mangoKiwi:
-            return "망키쥬스"
-        }
-    }
-    
-    var recipe: [Fruit : Int] {
-        switch self {
-        case .strawberry:
-            return [.strawberry: 16]
-        case .banana:
-            return [.banana: 2]
-        case .kiwi:
-            return [.kiwi: 3]
-        case .pineapple:
-            return [.pineapple: 2]
-        case .mango:
-            return [.mango: 3]
-        case .strawberryBanana:
-            return [.strawberry: 10, .banana: 1]
-        case .mangoKiwi:
-            return [.mango: 2, .kiwi: 1]
-        }
-        
-    }
+struct Juice {
+  let name: String
+  let recipe: Recipe
 }
