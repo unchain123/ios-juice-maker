9d0a70ea36d15b502225074bbbf0dd97b0b1b617
unchain123 <unchain123@kakao.com>
2023-03-17 11:17:18 +0900

    refactor: storyboard를 SwiftUI로 변경하기 위해 초기 세팅

---
+++ JuiceMaker/JuiceMaker/Controller/SwiftUIMainView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIMainView.swift
+//  JuiceMaker
+//
+//  Created by unchain on 2023/03/17.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+
